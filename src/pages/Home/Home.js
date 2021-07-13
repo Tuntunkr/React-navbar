615d2ff55fc7216c2db250913c4be75d6f4aead2
Tuntun <74050853+Tuntunkr@users.noreply.github.com>
2021-07-13 12:32:37 +0530

    Add files via upload

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+const Home = () => {
+  return (
+    <div className="container">
+    
+    <h1 className="text-center" style={{paddingTop: "30%"}}>
+      <img className="abc" src="image/img1.jpg" alt="a"></img>
+      
+    </h1>
+    
+  </div>
+  )
+}
+export default Home;
